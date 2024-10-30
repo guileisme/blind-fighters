7d0b98149726d944a6fc73c562c405e3df11135e
Guilherme <guileisme1@gmail.com>
2024-10-29 23:29:08 -0300

    Implementação da automatização para identificação dos jogadores na cena

--- Assets/Projeto_SNCT/AudioRumbleManager.cs
+++ Assets/Projeto_SNCT/AudioRumbleManager.cs
@@ -25,12 +25,37 @@ public class audioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // código de adriano para encontrar os personagens na cena 
+        if (player1 == null)
+        {
+            string[] playerNames = { "Fighter1(Keyboard:/Keyboard)_player", "Fighter2(Keyboard:/Keyboard)_player", "Fighter3(Keyboard:/Keyboard)_player", "Fighter4(Keyboard:/Keyboard)_player", "Fighter5(Keyboard:/Keyboard)_player" };
+            foreach (string name in playerNames)
+            {
+                GameObject foundPlayer = GameObject.Find(name);
+                if (foundPlayer != null)
+                {
+                player1 = foundPlayer;
+                }
+            }
+        }
+        if (player2 == null)
+        {
+            string[] playerNames2 = { "Fighter1()_player", "Fighter2()_player", "Fighter3()_player", "Fighter4()_player", "Fighter5()_player" };
+            foreach (string name in playerNames2)
+            {
+                GameObject foundPlayer2 = GameObject.Find(name);
+                if (foundPlayer2 != null)
+                {
+                player2 = foundPlayer2;
+                }
+            }
+        }
         if (player2.transform.position.x - player1.transform.position.x >= 0)
         {
             audioSourceDireita.mute = false;
             audioSourceEsquerda.mute = true;
             audioSourceAcima.mute = true;
-            Gamepad.current.SetMotorSpeeds(0f, 5f);
+            // Gamepad.current.SetMotorSpeeds(0f, 5f);
             // Debug.LogWarning("inimigo à direita");
             // naDireita = true;
             // naEsquerda = false;
@@ -42,7 +67,7 @@ public class audioManager : MonoBehaviour
             audioSourceDireita.mute = true;
             audioSourceEsquerda.mute = false;
             audioSourceAcima.mute = true;
-            Gamepad.current.SetMotorSpeeds(5f, 0f);
+            // Gamepad.current.SetMotorSpeeds(5f, 0f);
             // audioSource.Stop();
             // audioSource.clip = esquerdaClip;
             // Debug.LogWarning("inimigo à esquerda");
